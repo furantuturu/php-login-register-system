8150c9aa47f3f3140d3553d45958e34e8fc0dfff
Haemoffy <fuwansuu@gmail.com>
2024-07-29 20:06:32 +0800

    component for header and footer content

---
+++ views/partials/footer.php
@@ -0,0 +1,11 @@
+        <footer id="footer">
+            <ul class="copyright">
+                <li>Template Credits: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </footer>
+
+    <!-- Scripts -->
+        <script src="resource/assets/js/main.js"></script>
+
+</body>
+</html>
