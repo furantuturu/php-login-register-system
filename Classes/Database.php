edab0b8050998044f02f24545e52016ea5cfe961
Haemoffy <fuwansuu@gmail.com>
2024-07-29 20:05:01 +0800

    class for database setup

---
+++ Classes/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Classes;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $connection;
+    private $statement;
+    public function __construct($dsnData, $username = 'root', $password = '')
+    {
+        try {
+
+            $dsn = 'mysql:' . http_build_query($dsnData, '', ';');
+
+            $this->connection = new PDO($dsn, $username, $password, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+    
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+        } catch (PDOException $e) {
+
+            dieDump($e->getMessage());
+
+        }
+    }
+
+    public function query($query, $params = [])
+    {
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
+
+        return $this;
+    }
+}
