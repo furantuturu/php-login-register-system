b6f95eb999c6ae01cd010e6cc5a99ed3e4c47094
Haemoffy <fuwansuu@gmail.com>
2024-07-29 20:09:21 +0800

    implemeting and setup-ing files to run the app

--- public/index.php
+++ public/index.php
@@ -2,3 +2,29 @@
 
 const BASE_PATH = __DIR__ . '/../';
 
+require BASE_PATH . 'functions.php';
+
+vendorAutoloader();
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
+
+spl_autoload_register(function ($class) {
+
+    //* e.g Classes\Database becomes Classes/Database
+    $result = str_replace('\\', '/', $class);
+
+    require basePath("{$result}.php");
+
+});
+
+require basePath('bootstrap.php');
+
+$router = new Classes\Router;
+
+require basePath('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
