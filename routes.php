80071d2c46c1a5430b3ba1a244d2e99cca544cac
furantuturu <fuwansuu@gmail.com>
2024-07-30 18:03:50 +0800

    added routes for login, register and dashboard

--- routes.php
+++ routes.php
@@ -1,3 +1,12 @@
 <?php
 
 $router->get('/', 'index.php');
+$router->get('/dashboard', 'dashboard.php');
+
+//* Login
+$router->get('/login', 'login/create.php');
+$router->post('/login', 'login/store.php');
+
+//* Register
+$router->get('/register', 'registration/create.php');
+$router->post('/register', 'registration/store.php');
