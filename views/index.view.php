5cdc6fd31c603ac236fda696be43259281bf1014
furantuturu <fuwansuu@gmail.com>
2024-07-30 18:12:45 +0800

    added nav component, renamed signup to register

--- views/index.view.php
+++ views/index.view.php
@@ -1,13 +1,21 @@
 <?php require 'partials/head.php'?>
 
+<nav id="nav">
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+    </ul>
+</nav>
+
 <header id="header">
     <h1>Hey!</h1>
     <p>Welcome to my website, netizen!<br />
     You're free to try out my own Login and Register system.</p>
 </header>
 
-<form id="signup-form" method="post" action="#">
-    <input type="submit" value="Sign Up" />
-</form>
+<a href="/register">
+    <input type="button" value="Register" />
+</a> 
 
 <?php require 'partials/footer.php'?>
