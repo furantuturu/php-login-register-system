bbc5aeec282de806c33a5fe9d77fb08efd0609cd
furantuturu <fuwansuu@gmail.com>
2024-07-30 18:13:11 +0800

    built the login page

---
+++ views/login/create.view.php
@@ -0,0 +1,27 @@
+<?php require basePath('views/partials/head.php')?>
+
+<nav id="nav">
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/register">Register</a></li>
+    </ul>
+</nav>
+
+<div class="form-container">
+    <h2>Login</h1>
+    <form action="/login" method="post">
+        <div class="form-div">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Email Address" required>
+        </div>
+        <div class="form-div">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" required>
+        </div>
+        <div class="button-center">
+            <input type="submit" value="Login">
+        </div>
+    </form>
+</div>
+
+<?php require basePath('views/partials/footer.php')?>
