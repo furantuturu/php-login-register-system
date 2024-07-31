4e3db5429822cafd8a64290d19e478f9abd113fb
Haemoffy <fuwansuu@gmail.com>
2024-07-31 21:15:54 +0800

    added dashboard.css

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,12 +1,13 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
     <title><?= $title ?></title>
-    <meta charset="utf-8" />
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="resource/assets/css/nav.css" />
     <link rel="stylesheet" href="resource/assets/css/main.css" />
+    <link rel="stylesheet" href="resource/assets/css/nav.css" />
     <link rel="stylesheet" href="resource/assets/css/form.css" />
+    <link rel="stylesheet" href="resource/assets/css/dashboard.css" />
 </head>
 <body class="is-preload">
     
