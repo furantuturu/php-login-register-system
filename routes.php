445b46cb4c96917470cfea3eece7f9d866f7d233
Haemoffy <fuwansuu@gmail.com>
2024-07-31 21:06:01 +0800

    route update: extended the functionality with 'only' to check user type added logout route

--- routes.php
+++ routes.php
@@ -1,12 +1,15 @@
 <?php
 
 $router->get('/', 'index.php');
-$router->get('/dashboard', 'dashboard.php');
+$router->get('/dashboard', 'dashboard.php')->only('auth');
 
 //* Login
-$router->get('/login', 'login/create.php');
-$router->post('/login', 'login/store.php');
+$router->get('/login', 'login/create.php')->only('guest');
+$router->post('/login', 'login/store.php')->only('guest');
 
 //* Register
-$router->get('/register', 'registration/create.php');
-$router->post('/register', 'registration/store.php');
+$router->get('/register', 'registration/create.php')->only('guest');
+$router->post('/register', 'registration/store.php')->only('guest');
+
+//*Logout
+$router->delete('/logout', 'logout.php')->only('auth');
